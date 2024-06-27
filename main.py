d67f4d47ab996d0c9d1efe4ba1eba631b655cd11
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-06-28 01:00:45 +0500

    Viewing International Space Station on Google Maps

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World")
+import requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+print(response.status_code)
+response.raise_for_status()
+
+data = response.json()
+print(data)
+
+latitude = str(data["iss_position"]["latitude"])
+longitude = str(data["iss_position"]["longitude"])
+
+iss_position = latitude +","+ longitude
+
+url = "https://maps.google.com/maps?&daddr="
+
+google_map_link = url + iss_position
+print(google_map_link)
